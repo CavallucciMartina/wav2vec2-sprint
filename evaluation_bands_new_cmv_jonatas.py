f5bfe4a588f246ba5cf826543482e182a9acec6a
CavallucciMartina <martina.cavallucci96@gmail.com>
2022-02-05 16:30:54 +0000

    Evaluation cmv new

--- evaluation_bands_new_cmv_jonatas.py
+++ evaluation_bands_new_cmv_jonatas.py
@@ -1,12 +1,48 @@
-import torch
-import librosa
 import re
+from os.path import isfile, join
+from os import listdir, walk
+from datasets import Dataset, load_dataset, load_metric, concatenate_datasets
+import torchaudio
+import librosa
+import numpy as np
+from transformers import Wav2Vec2Processor
+from transformers import Wav2Vec2ForCTC
+import torch 
+import random
+import pandas as pd
+import csv
 import warnings
-from datasets import load_dataset, load_metric
-from transformers import Wav2Vec2ForCTC, Wav2Vec2Processor
 from jiwer import cer
 import jiwer
-import torchaudio
+
+print("#### NEW CMMV")
+
+def create_new_cmmv(path_to_csv, audio_folder):
+    dict = {"path": [], "sentence": []}
+    with open(path_to_csv, 'r') as f:
+
+        read_tsv = csv.reader(f, delimiter="\t")
+        read_tsv = [row for row in read_tsv]
+        read_tsv = read_tsv[1:]
+        for row in read_tsv:
+            dict["path"].append(f"{audio_folder}{row[1]}")
+            dict["sentence"].append(row[2])
+    return Dataset.from_dict(dict)
+
+print("#### Loading dataset")
+
+print("####Commonvoice new")
+
+fold = "../cv_new/cv-corpus-7.0-2021-07-21/it/"
+
+
+common_voice_train = create_new_cmmv(f"{fold}train2.csv", f"{fold}clips2/")
+common_voice_val = create_new_cmmv(f"{fold}dev2.csv", f"{fold}clips2/")  
+
+print("####Merge datasets")
+
+test_dataset = concatenate_datasets([common_voice_train, common_voice_val]).shuffle(seed=1234)
+
 
 LANG_ID = "it"
 MODEL_ID = "jonatasgrosman/wav2vec2-large-xlsr-53-italian"
@@ -17,8 +53,6 @@ CHARS_TO_IGNORE = [",", "?", "¿", ".", "!", "¡", ";", "；", ":", '""', "%", '
                    "{", "}", "=", "`", "_", "+", "<", ">", "…", "–", "°", "´", "ʾ", "‹", "›", "©", "®", "—", "→", "。",
                    "、", "﹂", "﹁", "‧", "～", "﹏", "，", "｛", "｝", "（", "）", "［", "］", "【", "】", "‥", "〽",
                    "『", "』", "〝", "〟", "⟨", "⟩", "〜", "：", "！", "？", "♪", "؛", "/", "\\", "º", "−", "^", "ʻ", "ˆ"]
-
-test_dataset = load_dataset("common_voice", LANG_ID, split="test")
 
 wer = load_metric("wer.py") # https://github.com/jonatasgrosman/wav2vec2-sprint/blob/main/wer.py
 cer = load_metric("cer") #For CER import from jiwer because jonatas code generated error
@@ -81,8 +115,8 @@ for index, batch in enumerate(test_dataset):
 total_wer /= len(test_dataset)
 total_cer /= len(test_dataset)
 
-with open(f"evaluation_bands_cmmv_jonatas.txt", "w") as f:
-    f.write("Common Voice Evaluation with jonatasgrosman/wav2vec2-large-xlsr-53-italian on test set \n")
+with open(f"evaluation_bands_newcmv_total_jonatas.txt", "w") as f:
+    f.write("New Common Voice Dataset Evaluation with jonatasgrosman/wav2vec2-large-xlsr-53-italian on train and valiadtion set \n")
     for key in sorted(ranges.keys()):
         mean_wer = ranges[key][1] / ranges[key][0]
         mean_cer = ranges[key][2] /ranges[key][0]
